e6e8fa652b65fbdd874d6a8c307c9348d14fae55
Guilherme Welter <guilherme.a.welter@gmail.com>
2024-10-06 19:10:15 -0300

    Error thandling with stderr

--- geo2json.c
+++ geo2json.c
@@ -8,18 +8,18 @@ int main() {
 
   puts("{\"data\":["); // fist line of json
   while (scanf("%f,%f,%79[^\n]", &latitude, &longitude, info) == 3) {
+    if (latitude < -90 || latitude > 90) {
+      fprintf(stderr, "Invalid latitude: %f. Skipping line\n", latitude);
+      continue;
+    }
+    if (longitude < -180 || longitude > 180) {
+      fprintf(stderr, "Invalid longitude: %f. Skipping line\n", longitude);
+      continue;
+    }
     if (started) {
       printf(",\n");
     } else {
       started = 1;
-    }
-    if (latitude < -90 || latitude > 90) {
-      printf("Invalid latitude: %f", latitude);
-      return 2;
-    }
-    if (longitude < -180 || longitude > 180) {
-      printf("Invalid longitude: %f", longitude);
-      return 2;
     }
     printf("{\"latitude\": %f, \"longitude\": %f, \"info\": \"%s\"}", latitude,
            longitude, info);
